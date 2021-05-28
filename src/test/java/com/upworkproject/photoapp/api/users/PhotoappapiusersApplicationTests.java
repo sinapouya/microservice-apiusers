ba411054e498fc172e16c88b8f9f0b47758434b1
s.pouya <s.pouya@pinket.ir>
2021-05-28 17:45:37 +0430

    first commit

---
+++ src/test/java/com/upworkproject/photoapp/api/users/PhotoappapiusersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upworkproject.photoapp.api.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappapiusersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
